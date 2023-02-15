1ce78c15cc354023d1a12e25d3a0e13c2fc11315
Stas <stas.shc@gmail.com>
2023-02-15 15:52:10 +0300

    Parser CSV with Scanner

--- src/Main.java
+++ src/Main.java
@@ -18,20 +18,17 @@ public class Main {
 
     public static void main(String[] args) throws IOException, CsvValidationException, FileNotFoundException {
 
+        Scanner in = new Scanner("files/input.csv");
+        while (in.hasNext()){
+            Reader reader = Files.newBufferedReader(Paths.get(in.next()));
+            CSVReader csvReader = new CSVReader(reader);
 
-        try (Reader reader = Files.newBufferedReader(Paths.get("files/input.csv"));
-             CSVReader csvReader = new CSVReader(reader)) {
-
-            // read one record at a time
             String[] record;
             while ((record = csvReader.readNext()) != null) {
                 System.out.println(String.join(", ", record));
             }
 
-        } catch (IOException | CsvValidationException ex) {
-            System.err.println(ex.getMessage());
         }
-
 
     }
 }
