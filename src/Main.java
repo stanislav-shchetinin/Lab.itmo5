f48055f46fd9900aa3a3243a296f36f02e04c718
Stas <stas.shc@gmail.com>
2023-02-15 13:05:58 +0300

    Clean

--- src/Main.java
+++ src/Main.java
@@ -17,10 +17,7 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) throws IOException, CsvValidationException, FileNotFoundException {
-
-        //Scanner in = new Scanner(Path.of("files/input.csv"), StandardCharsets.UTF_8);
-        //PriorityQueue<Vehicle> priorityQueue = FileRead.fileRead(Parse.parseFromCSVtoString(in));
-        //System.out.println(Parse.parseFromCSVtoString(in));
+        
 
         try (Reader reader = Files.newBufferedReader(Paths.get("files/input.csv"));
              CSVReader csvReader = new CSVReader(reader)) {
