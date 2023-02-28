d5e51631dda6b1ebc1a9addb5ee1bc71347ede3d
Stas <stas.shc@gmail.com>
2023-02-28 20:10:47 +0300

    create LoggerForCommands

--- src/main/java/commands/Exit.java
+++ src/main/java/commands/Exit.java
@@ -1,15 +1,18 @@
 package commands;
 
 import console.Console;
+import service.LoggerForCommands;
 import service.command.Command;
 
 import java.util.logging.Logger;
 
+import static service.LoggerForCommands.loggerInfo;
+
 public class Exit implements Command {
-    private static final Logger logger = Logger.getLogger(Exit.class.getName());
+
     @Override
     public void execute() {
-        logger.info("Программа завершает выполнение: была вызвана команда exit");
+        loggerInfo("Программа завершает выполнение: была вызвана команда exit");
         System.exit(0);
     }
 }
---
+++ src/main/java/service/LoggerForCommands.java
@@ -0,0 +1,15 @@
+package service;
+
+import commands.Exit;
+
+import java.util.logging.Logger;
+
+public class LoggerForCommands {
+    private static final Logger logger = Logger.getLogger(LoggerForCommands.class.getName());
+    public static void loggerInfo(String messege){
+        logger.info(messege);
+    }
+    public static void loggerWarning(String messege){
+        logger.info(messege);
+    }
+}
