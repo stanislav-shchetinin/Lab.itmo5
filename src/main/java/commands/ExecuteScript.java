6d878dc8264774d9e8c50dd2a76773718eb32fc9
Stas <stas.shc@gmail.com>
2023-03-13 15:56:00 +0300

    update InitMap

--- src/main/java/commands/ExecuteScript.java
+++ src/main/java/commands/ExecuteScript.java
@@ -1,4 +1,22 @@
 package commands;
 
-public class ExecuteScript {
+import service.CollectionClass;
+import service.command.Command;
+
+public class ExecuteScript implements Command {
+
+    private CollectionClass collectionClass;
+
+    public ExecuteScript(CollectionClass collectionClass){
+        this.collectionClass = collectionClass;
+    }
+    @Override
+    public void execute() {
+
+    }
+
+    @Override
+    public void getParametr() {
+
+    }
 }
--- src/main/java/service/command/InitMap.java
+++ src/main/java/service/command/InitMap.java
@@ -13,7 +13,19 @@ public class InitMap {
         map.put("help", new Help());
         map.put("info", new Info(collectionClass));
         map.put("show", new Show(collectionClass));
-        map.put("add", new AddElement());
+        map.put("add", new AddElement(collectionClass));
+        map.put("update_id", new UpdateId(collectionClass));
+        map.put("remove_by_id", new RemoveById(collectionClass));
+        map.put("clear", new Clear(collectionClass));
+        map.put("save", new Save(collectionClass));
+        map.put("execute_script", new ExecuteScript(collectionClass));
+        map.put("exit", new Exit());
+        map.put("remove_first", new RemoveFirst(collectionClass));
+        map.put("add_if_max", new AddIfMax(collectionClass));
+        map.put("add_if_min", new AddIfMin(collectionClass));
+        map.put("count_by_capacity", new CountByCapacity(collectionClass));
+        map.put("print_ascending", new PrintAscending(collectionClass));
+        map.put("print_unique_engine_power", new PrintUniqueEnginePower(collectionClass));
         return map;
     }
 
